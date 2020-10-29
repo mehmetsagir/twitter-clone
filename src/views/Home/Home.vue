be36fb626c32691691c25ae8be4f4ce596f50ebf
Mehmet Sağır <62952843+mehmetsagir@users.noreply.github.com>
2020-10-30 02:46:20 +0300

    Trends for you component created

---
+++ src/views/Home/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="page">
+     <HomeHeader/>
+     <div class="pageContainer">
+        <tweetSendArea/>
+        <div class="line"></div>
+        <Tweet/>
+     </div>
+  </div>
+</template>
+
+<script>
+import HomeHeader from "@/views/Home/Shared/HomeHeader";
+import TweetSendArea from "@/components/tweetSendArea";
+import Tweet from "@/components/Tweet";
+export default {
+    name: "Home",
+   components: {Tweet, TweetSendArea, HomeHeader}
+};
+</script>
+
+<style scoped lang="scss">
+.page{
+   width: 598px;
+}
+.pageContainer{
+   width: 598px;
+   border-right: 1px solid #38444d;
+}
+.line{
+   margin-top: auto;
+   width: 100%;
+   height: 10px;
+   background: rgb(37, 51, 65);
+}
+</style>
---
+++ src/views/Home/Shared/HomeHeader.vue
@@ -0,0 +1,41 @@
+<template>
+   <div class="container">
+      <CustomText font="xlarge fw-heavy">Home</CustomText>
+      <icon class="stars hover" name="stars" />
+   </div>
+</template>
+
+<script>
+import CustomText from "@/components/CustomText";
+export default {
+name: "HomeHeader",
+   components: {CustomText}
+}
+</script>
+
+<style scoped lang="scss">
+   .container{
+      background: #15202B;
+      width: 100%;
+      height: 53px;
+      border-bottom: 1px solid #38444d;
+      border-right: 1px solid #38444d;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      position: sticky;
+      top: 0;
+      padding: 0 5px 0 15px;
+      z-index: 99;
+      span{
+         cursor: pointer;
+      }
+
+      .stars{
+         width: 40px;
+         fill: #1da1f2;
+         border-radius: 50%;
+         padding: 8px;
+      }
+   }
+</style>
